ce71b45784806abbff0181a548b4b01efdc78993
SheHuan <she320955huan>
2022-02-17 22:10:00 +0800

    ReorderList-3

--- src/main/java/com/sh/linklist/ReorderList.java
+++ src/main/java/com/sh/linklist/ReorderList.java
@@ -1,7 +1,5 @@
 package com.sh.linklist;
 
-import jdk.javadoc.internal.doclets.formats.html.markup.Head;
-
 /**
  * 给你一个链表，链表中结点的顺序是L0→ L1→ L2→…→ Ln-1→ Ln，
  * 请问如何重排链表使得结点的顺序变成L0→ Ln→ L1→ Ln-1→ L2→ Ln-2→…？
@@ -32,8 +30,29 @@ public class ReorderList {
     }
 
     private Node reorder(Node head1, Node head2) {
-//        Node dummy = new Node(0);
-//        dummy.next = head1;
+        // 保存第一个链表的头节点，也就是最终新链表的头节点
+        Node head = head1;
+        // 定义一个临时空节点，将两个链表串起来
+        Node temp = new Node(0);
+        while (head1 != null && head2 != null) {
+            // 先保存第一个链表当前节点的next
+            Node head1Next = head1.next;
+
+            // 第一次是空节点指向第一个链表的头节点，后续是从第二个链表的头节点开始分别指向第一个链表的后续节点
+            temp.next = head1;
+            // 第一个链表的第n个节点指向第二个链表的第n个节点
+            head1.next = head2;
+            temp = head2;
+
+            // 两个链表头指针分别后移一步
+            head1 = head1Next;
+            head2 = head2.next;
+        }
+        // 由于第一个链表可能比第二个多一个节点，所以循环结束后第一个链表可能剩一个节点没处理
+        if (head1 != null) {
+            temp.next = head1;
+        }
+        return head;
     }
 
     public static void main(String[] args) {
